b00fd15e5058018b113b4179cd85bed8f0a5f706
yso0131 <souchenmin@icloud.com>
2021-03-14 23:26:41 +0900

    back everything original

--- App.jsx
+++ App.jsx
@@ -1,44 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
 import TopBar from './src/components/TopBar';
 import Post from './src/screens/Post';
 import Mypage from './src/screens/Mypage';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
-
-const Tab = createBottomTabNavigator();
-
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <View style={styles.container}>
-          <TopBar />
-          <Post />
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="Settings" component={SettingsScreen} />
-          {/* eslint-disable-next-line */}
+    <View style={styles.container}>
+      <TopBar />
+      <Mypage />
+      {/* eslint-disable-next-line */}
       <StatusBar style="auto" />
-        </View>
-      </Tab.Navigator>
-    </NavigationContainer>
+    </View>
   );
 }
 
